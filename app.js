497c7a2144d38d621eb1d57cc5d0bb4732aa325e
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-04 06:44:04 -0800

    Add files via upload
    6th js assgiment

---
+++ app.js
@@ -0,0 +1,42 @@
+// MATH EXPRESSIONS Assignment # 6 JAVASCRIPT
+// question 1
+
+
+
+// question 1
+// document.write("<h1>Result")
+// var a = 10;
+// var result;
+
+//  document.write( "Value of a = " + a );
+
+// var a = 11;
+// var result;
+// document.write("the Value of ++a is 11: " + ++num1);
+
+
+// var b = 12;
+// var result;
+// document.write("the Value of b++ is 12: " + b++);
+
+
+// var a = 13;
+// var result;
+// document.write("the Value of --a is 13: " + --a);
+
+// var b = 14;
+// var result;
+// document.write("the Value of --a is 14: " + b--);
+
+// question 2
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+            
+// console.log(result)
+
+// document.write(result);
+// alert(result);
+
+
